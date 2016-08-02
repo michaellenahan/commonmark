cd6d0720e9bc0c9b526b4d1b18a8a0e6decab208
Michael Lenahan <michael.lenahan@erdfisch.de>
2016-08-02 17:57:27 +0200

    Cleanup

--- src/Plugin/Filter/CommonMark.php
+++ src/Plugin/Filter/CommonMark.php
@@ -3,22 +3,22 @@
 namespace Drupal\commonmark\Plugin\Filter;
 
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Link;
+use Drupal\Core\Url;
 use Drupal\filter\Plugin\FilterBase;
 use Drupal\filter\FilterProcessResult;
 use League\CommonMark\CommonMarkConverter;
 
 /**
  * Provides a filter to process CommonMark Markdown to HTML.
- * 
+ *
  * @Filter(
  *   id = "commonmark",
  *   title = @Translation("CommonMark"),
  *   description = @Translation("Convert Markdown to HTML with CommonMark."),
  *   type = Drupal\filter\Plugin\FilterInterface::TYPE_MARKUP_LANGUAGE,
  *   settings = {
- *     "allowed_tags" = "<a> <abbr> <address> <blockquote> <br> <cite> <code> <dd> <del> <div> <dl> <dt> <em> <h1> <h2> <h3> <h4> <h5> <h6> <hr> <img> <kbd> <li> <ol> <p> <pre> <small> <span> <strong> <sub> <sup> <table> <tbody> <td> <tfoot> <th> <thead> <tr> <ul>",
- *     "show_examples" = TRUE,
- *     "use_global_settings" = TRUE
+ *     "commonmark_help" = TRUE
  *   }
  * )
  */
@@ -28,23 +28,10 @@ class CommonMark extends FilterBase {
    * {@inheritdoc}
    */
   public function settingsForm(array $form, FormStateInterface $form_state) {
-    $form['allowed_tags'] = array(
-      '#type' => 'textfield',
-      '#title' => $this->t('Allowed HTML tags'),
-      '#default_value' => $this->settings['allowed_tags'],
-      '#maxlength' => 2048,
-      '#size' => 250,
-      '#description' => $this->t('<p>A list of HTML tags that will be permitted to remain after CommonMark has converted the Markdown to HTML. JavaScript event attributes, JavaScript URLs, and CSS are always stripped.</p><p><strong>WARNING: Modifying this list will have a severe impact on the final output from CommonMark, use with caution.</strong></p>'),
-    );
-    $form['show_examples'] = array(
+    $form['commonmark_help'] = array(
       '#type' => 'checkbox',
-      '#title' => $this->t('Show examples in the "long" filter tips.'),
-      '#default_value' => $this->settings['show_examples'],
-    );
-    $form['use_global_settings'] = array(
-      '#type' => 'checkbox',
-      '#title' => $this->t('Use global CommonMark settings.'),
-      '#default_value' => $this->settings['use_global_settings'],
+      '#title' => $this->t('Display basic CommonMark help in long filter tips.'),
+      '#default_value' => $this->settings['commonmark_help'],
     );
 
     return $form;
@@ -54,8 +41,22 @@ class CommonMark extends FilterBase {
    * {@inheritdoc}
    */
   public function process($text, $langcode) {
+    // It is necessary to escape html input.
+    // See: https://commonmark.thephpleague.com/security/
     $converter = new CommonMarkConverter(['html_input' => 'escape']);
     $processed_text = $converter->convertToHtml($text);
     return new FilterProcessResult($processed_text);
   }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function tips($long = FALSE) {
+    $url_options = array('attributes' => array('target' => '_blank'));
+    $url = Url::fromUri('http://commonmark.org/help/', $url_options);
+    $text = $this->t('This site uses Markdown. Click here for help.');
+    $link = Link::fromTextAndUrl($text, $url)->toString();
+    return $link;
+  }
+
 }
